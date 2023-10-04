bb277f83ac28f7d50d8aacfe886e93b3e078e6a5
rina1309 <113968192+rina1309@users.noreply.github.com>
2023-10-04 12:57:17 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,12 +12,12 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <Header /> {/* Включаем хедер здесь */}
+        <Header /> 
         <Routes>
-          <Route path="/" element={<Home />} /> {/* Главная страница */}
-          <Route path="/form" element={<Form />} /> {/* Страница с формой */}
-          <Route path="/twoBlocks" element={<TwoBlocks />} /> {/* Страница со слайдером */}
-          <Route path="/todolist" element={<TodoList />} /> {/* Страница с ToDo-листом */}
+          <Route path="/" element={<Home />} /> 
+          <Route path="/form" element={<Form />} /> 
+          <Route path="/twoBlocks" element={<TwoBlocks />} /> 
+          <Route path="/todolist" element={<TodoList />} /> 
         </Routes>
       </div>
     </Router>
